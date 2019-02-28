16d361a1d1e3ea7a03a6a3d223f59bc07503224b
Davis Haba <davis.haba76@gmail.com>
2019-02-27 23:04:49 -0800

    update th to use full path of binary

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def gen_img(batch_size: int, img_size: int, model_name: str, noise_mode: str) ->
 
     # TODO extra validation for noise modes that only do batch size of 1???
 
-    cmd = "cd {} && gpu=1 batchSize={} net={} imsize={} name={} display=0 noisemode={} th generate.lua"\
+    cmd = "cd {} && gpu=1 batchSize={} net={} imsize={} name={} display=0 noisemode={} /home/dhaba/torch/install/bin/th generate.lua"\
           .format(PROJ_DIR, batch_size, latest_cp, img_size, f_name, noise_mode)
 
     print("Executing cmd:\n{}".format(cmd))
